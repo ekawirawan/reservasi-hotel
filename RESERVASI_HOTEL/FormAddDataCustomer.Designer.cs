aabadcbce34825382807aae4c4b83a3bcdafa994
artesia0528 <120473425+artesia0528@users.noreply.github.com>
2023-12-20 23:14:43 +0800

    Desain customer 1

---
+++ RESERVASI_HOTEL/FormAddDataCustomer.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace RESERVASI_HOTEL
+{
+    partial class FormAddDataCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSimpanDataCustomer = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNoTelp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAlamatCustomer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNamaCustomer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSimpanDataCustomer
+            // 
+            this.btnSimpanDataCustomer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSimpanDataCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimpanDataCustomer.Location = new System.Drawing.Point(458, 309);
+            this.btnSimpanDataCustomer.Name = "btnSimpanDataCustomer";
+            this.btnSimpanDataCustomer.Size = new System.Drawing.Size(111, 47);
+            this.btnSimpanDataCustomer.TabIndex = 14;
+            this.btnSimpanDataCustomer.Text = "Simpan";
+            this.btnSimpanDataCustomer.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 18);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "No Telphone";
+            // 
+            // txtNoTelp
+            // 
+            this.txtNoTelp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNoTelp.Location = new System.Drawing.Point(157, 211);
+            this.txtNoTelp.Name = "txtNoTelp";
+            this.txtNoTelp.Size = new System.Drawing.Size(392, 22);
+            this.txtNoTelp.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Alamat";
+            // 
+            // txtAlamatCustomer
+            // 
+            this.txtAlamatCustomer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtAlamatCustomer.Location = new System.Drawing.Point(157, 141);
+            this.txtAlamatCustomer.Name = "txtAlamatCustomer";
+            this.txtAlamatCustomer.Size = new System.Drawing.Size(392, 22);
+            this.txtAlamatCustomer.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nama";
+            // 
+            // txtNamaCustomer
+            // 
+            this.txtNamaCustomer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNamaCustomer.Location = new System.Drawing.Point(157, 82);
+            this.txtNamaCustomer.Name = "txtNamaCustomer";
+            this.txtNamaCustomer.Size = new System.Drawing.Size(392, 22);
+            this.txtNamaCustomer.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Tambah Data Customer";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // FormAddDataCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 384);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSimpanDataCustomer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNoTelp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAlamatCustomer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNamaCustomer);
+            this.Name = "FormAddDataCustomer";
+            this.Text = "FormAddDataCustomer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSimpanDataCustomer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNoTelp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAlamatCustomer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNamaCustomer;
+        private System.Windows.Forms.Label label4;
+    }
+}
